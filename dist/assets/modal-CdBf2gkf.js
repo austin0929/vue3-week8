145648474fd53deb3b921f2dc29fb17435babe84
austin0929 <austin0929@gmail.com>
2024-03-24 23:20:16 +0800

    修改base

--- dist/assets/modal-CdBf2gkf.js
+++ dist/assets/modal-CdBf2gkf.js
@@ -1,4 +1,4 @@
-import{J as x,K as gt}from"./index-BOfdHV1z.js";var pt={exports:{}},z={exports:{}},j={exports:{}};/*!
+import{J as x,K as gt}from"./index-CPAp1LdQ.js";var pt={exports:{}},z={exports:{}},j={exports:{}};/*!
   * Bootstrap data.js v5.3.3 (https://getbootstrap.com/)
   * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
